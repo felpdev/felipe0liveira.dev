9ae0df29f8f1469ce9e63a96f44dbb0acb43f19b
Felipe 0liveira <felipe.araujo@nubank.com.br>
2021-06-09 23:20:10 -0300

    feat(): Main Page was born

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'felipe0liveira.dev',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'felipe0liveira.dev'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -46,68 +25,117 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/images/avatar.jpg',
+                    width: 150,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  child: Column(
+                    children: [
+                      Text('FELIPE OLIVEIRA'),
+                      const SizedBox(height: 2),
+                      Text(
+                        'SOFTWARE ENGINEER',
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+                Section(
+                  body: Text(
+                      'Seja absurdamente bem vindo à essa página que eu costumo chamar de meu sitezão, agora nessa v3, com um novo visual.\nClica em tudo, acha uns bugs tops e o mais importante, vai lá no Github e corrige, bjs.'),
+                ),
+                Section(
+                  body: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                          'https://media.giphy.com/media/3oriNKgISzB8diFeFy/giphy.gif'),
+                      const SizedBox(height: 15),
+                      Text(
+                        'Sobre Mim',
+                        style: TextStyle(fontSize: 22, color: Colors.grey[600]),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                          'Me chamo Felipe 0liveira, sou apaixonado por tecnologia de modo geral. Minha introdução no mundo da programação começou quando percebi meu brilho nos olhos em apoiar grandes ideias, com o objetivo facilitar a vida das pessoas e das empresas.\nMeu principal objetivo é prover soluções à nível do cliente, analisar suas necessidades e dores, me envolver na ideia e traçar o melhor plano de ação para a execução do desenvolvimento para superar positivamente as expectativas.'),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 150,
+                  child: ListView.builder(
+                      itemCount: 1244,
+                      // shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Section(
+                          body: Text('Menino ${index + 1}˚'),
+                          squared: true,
+                        );
+                      }),
+                ),
+                Section(
+                  body: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                          'https://media.giphy.com/media/rvjIGIOI9lhsc/giphy.gif'),
+                      const SizedBox(height: 15),
+                      Text(
+                        'Skills',
+                        style: TextStyle(fontSize: 22, color: Colors.grey[600]),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                          'Tenho uma régua altíssima de qualidade, principalmente se tratando de estruturação de código, organização de projetos, design patterns, etc.\nMe destaco tanto na comunicação assertiva com as pessoas, prezando objetividade e clareza, quanto por minha rápida e eficiente curva de aprendizado, entregando máxima performance dentro da expectativa de prazo.'),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class Section extends StatelessWidget {
+  final Widget? body;
+  final bool? squared;
+
+  const Section({
+    Key? key,
+    @required this.body,
+    this.squared,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Container(
+      width: (squared != null && squared == true)
+          ? 150
+          : MediaQuery.of(context).size.width,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: body,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
